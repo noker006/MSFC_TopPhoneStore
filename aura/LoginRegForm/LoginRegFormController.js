2afc9e0aeca75be0304159c500aab1c65e5ca2c6
Filonenko Maxim <maksim_filonenka@senla.eu>
2021-04-17 20:30:13 +0300

    Done regLog form

--- aura/LoginRegForm/LoginRegFormController.js
+++ aura/LoginRegForm/LoginRegFormController.js
@@ -5,58 +5,30 @@
 
 ({
     doInit: function (component, event, helper) {
-        component.find("leadEditor").getNewRecord(
-            "Lead",    // sObject type (objectApiName)
-            null,      // recordTypeId
-            false,     // skip cache?
-            $A.getCallback(function () {
-                var rec = component.get("v.leadRecord");
-                var error = component.get("v.leadEditorErrors");
-                if (error || (rec === null)) {
-                    console.log("Error initializing record template: " + error);
-                    return;
-                }
-                console.log("Record template initialized: " + rec.apiName);
-            })
-        );
+
     },
 
     login: function (cmp, event, helper) {
-        const password = cmp.get("v.lgPassword");
         const username = cmp.get("v.lgUsername");
+        const password = cmp.get("v.lgPassword");
 
         helper.checkUser(cmp, username, password);
     },
 
     register: function (cmp, event, helper) {
-        cmp.set("v.lgShowErrorMessage", false);
+        cmp.set("v.regShowErrorMessage", false);
         cmp.find('rgRegisterButton').set("v.disabled", true);
+
+        const lead = cmp.get("v.lead");
         let isValidRegData = helper.checkRegFormValidity(cmp);
-
+        console.log('1');
         if (isValidRegData) {
-            cmp.find("leadEditor").saveRecord(function (saveResult) {
-                cmp.find('rgRegisterButton').set("v.disabled", false);
-                let err;
-
-                if (saveResult.state === "SUCCESS" || saveResult.state === "DRAFT") {
-                    console.log("SUCCESS lead was saved.");
-                    cmp.set("v.regShowErrorMessage", false);
-                    helper.showToast(cmp, "SUCCESS", "success", "success");
-                } else if (saveResult.state === "INCOMPLETE") {
-                    err = "User is offline, device doesn't support drafts."
-                    helper.errorHanler(cmp, "Error", "You is offline", "##"+err);
-                } else if (saveResult.state === "ERROR") {
-                    err = "Problem saving lead, error: " + JSON.stringify(saveResult.error);
-                    helper.errorHanler(cmp, "Error", "Problem saving lead, error", "##"+err);
-                } else {
-                    err = "Unknown problem, state: " + saveResult.state + ", error: " + JSON.stringify(saveResult.error);
-                    helper.errorHanler(cmp, "Error", "Unknown problem", "##"+err);
-                }
-            });
+            console.log('2');
+            helper.createUser(cmp, lead);
         } else {
-            cmp.find("rgRegisterButton").set("v.disabled", false);
-
             helper.errorHanler(cmp, "Error", "You must fill in all the fields correctly", "##not all fields filled are correctly");
+
+            cmp.find("rgRegisterButton").set("v.disabled", false);
         }
     },
 
@@ -70,4 +42,8 @@
         cmp.set("v.showRegForm", false);
     },
 
+    regEmailIsChange: function (cmp, event, helper){
+        cmp.set("v.regShowErrorMessage", false);
+        cmp.find('rgRegisterButton').set("v.disabled", false);
+    },
 });
--- aura/LoginRegForm/LoginRegFormHelper.js
+++ aura/LoginRegForm/LoginRegFormHelper.js
@@ -19,9 +19,11 @@
                 let result = response.getReturnValue();
                 console.log('#######Result: ');
                 console.log(result);
-                if (result.isValidUser) {
-
+                if (result.isExistedUser) {
+                    this.showToast(cmp, "OK", "Hi"+ result.user.Name, "success");
                 } else {
+                    this.errorHanler(cmp, "Error", "result.errorMessage", "##"+result.errorMessage);
+
                     cmp.set("v.lgErrorMessage", result.errorMessage);
                     cmp.set("v.lgShowErrorMessage", true);
                 }
@@ -33,17 +35,54 @@
         $A.enqueueAction(action);
     },
 
-    handleActionError: function (cmp, response, state) {
-        console.error(state);
-        cmp.set("v.isLoadingDataTable", false);
-        let errors = response.getError();
-        if (errors) {
-            if (errors[0] && errors[0].message) {
-                console.log("Error message: " + errors[0].message);
+    createUser: function (cmp, lead) {
+        cmp.set("v.regShowErrorMessage", false);
+        console.log('3');
+        const action = cmp.get("c.createLead");
+        action.setParams({
+            "lead": lead
+        });
+        action.setCallback(this, function (response) {
+            const state = response.getState();
+            cmp.find('rgRegisterButton').set("v.disabled", false);
+            if (state === "SUCCESS") {
+                console.log('4');
+
+                let result = response.getReturnValue();
+                console.log('#######JJJJJJJJJJJJJJJJJJJJResult: ');
+                console.log(result);
+                if (!this.isBlank(result)) {
+                    this.showToast(cmp, "OK", result, "success");
+                } else {
+                    this.errorHanler(cmp, "Error", "User with the same email already exists", "##"+result.errorMessage);
+
+                    cmp.set("v.regErrorMessage", "User with the same email already exists");
+                    cmp.set("v.regShowErrorMessage", true);
+                }
+            } else if (state === "ERROR") {
+                this.handleActionError(cmp, response, state);
             }
-        } else {
-            console.log("Unknown error");
-        }
+        });
+        $A.enqueueAction(action);
+    },
+
+    showToast: function (cmp, title, message, type) {
+        var toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            "title": title,
+            "message": message,
+            "type": type
+        });
+        toastEvent.fire();
+    },
+
+    errorHanler: function (cmp, title, userMessage, logMessage) {
+        console.log(logMessage);
+
+        cmp.set("v.regShowErrorMessage", true);
+        cmp.set("v.regErrorMessage", userMessage);
+
+        this.showToast(cmp, title, userMessage, "error");
     },
 
     checkRegFormValidity: function (cmp) {
@@ -67,23 +106,21 @@
         return isValidRegData;
     },
 
-    showToast: function (cmp, title, message, type) {
-        var toastEvent = $A.get("e.force:showToast");
-        toastEvent.setParams({
-            "title": title,
-            "message": message,
-            "type": type
-        });
-        toastEvent.fire();
+    handleActionError: function (cmp, response, state) {
+        console.error(state);
+        cmp.set("v.isLoadingDataTable", false);
+        let errors = response.getError();
+        if (errors) {
+            if (errors[0] && errors[0].message) {
+                console.log("Error message: " + errors[0].message);
+            }
+        } else {
+            console.log("Unknown error");
+        }
     },
 
-    errorHanler: function (cmp, title, userMessage, logMessage) {
-        console.log(logMessage);
-
-        cmp.set("v.regShowErrorMessage", true);
-        cmp.set("v.regErrorMessage", userMessage);
-
-        this.showToast(cmp, title, userMessage, "error");
+    isBlank: function(str) {
+        return (!str || /^\s*$/.test(str));
     },
 
 });
